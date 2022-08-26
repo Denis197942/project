cff363c7dedc466454ab5106345f07a6f4584642
Denis <kokotov.denis@yandex.ru>
2022-08-26 11:20:42 +0700

    начинаем урок снова

--- src/js/urok1.js
+++ src/js/urok1.js
@@ -6,12 +6,12 @@ let num=56;
 //}
 
 do {
-    console.log (num);
+    console.log(num);
     num++;
 }
 
 while (num<55)
 
-for (let i=1; i<8; i++) {
-    console.log("i")
+for (let i = 1; i < 8; i++) {
+    console.log("i");
 }
